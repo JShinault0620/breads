03bae8649ebfb92b8782bf0affe2f7d1374bd415
JShinault0620 <jshincool@gmail.com>
2022-05-04 18:20:17 -0400

    Added delete functionality

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -15,7 +15,8 @@ breads.get('/', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
         res.render('Show', {
-            bread: Bread[req.params.arrayIndex]
+            bread: Bread[req.params.arrayIndex],
+            index: req.params.arrayIndex
         })
     } else {
         res.render('404', {
@@ -23,6 +24,12 @@ breads.get('/:arrayIndex', (req, res) => {
         })
         console.log(req.params.arrayIndex)
     }
+})
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+    Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
 })
 
 module.exports = breads
